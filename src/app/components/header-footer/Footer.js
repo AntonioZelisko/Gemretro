79601b3c30977710256569fe708f06d98ca13b07
AntonioZelisko <antonio.zelisko@gmail.com>
2020-09-16 12:15:32 +0200

    first commit

---
+++ src/app/components/header-footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="text-center">
+                <span className="text-muted">
+                    <a href='https://github.com/petermollet?tab=repositories'>@Peter Mollet</a> 
+                    - 2020 - 
+                    <a href='http://www.insy2s.com/'>INSY2S</a>
+                </span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
